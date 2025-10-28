74887952912c51361e05f431ab3f56316b884dd9
Prathmesh <prathmesh.malunjkar02@gmail.com>
2025-10-28 10:49:29 +0530

    as_aw

---
+++ async_await.js
@@ -0,0 +1,12 @@
+async function solve(){
+    try{
+        let response= await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        let data= await response.json();
+        console.log(data);
+    }
+    catch (error){
+        console.log(error);
+        
+    }
+}
+solve()
