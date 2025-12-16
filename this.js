fa4be97361322c48a6c9d109240c7827ad8506fd
Prathmesh <prathmesh.malunjkar02@gmail.com>
2025-12-16 17:51:04 +0530

    this js

---
+++ this.js
@@ -0,0 +1,13 @@
+var Person={
+    name:"Rohit",
+    age:45,
+    city:"Mumbai",
+    getname:function(){
+        console.log(this.name);
+        console.log(this.age);
+        
+    }
+}
+
+Person.getname()
+
